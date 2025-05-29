dcbadb5db23cbb7432efee85670147e7686335a6
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-05-30 01:38:00 +0200

    feat: add jpa annotations to the book and author class. Establish the relationship between the both.

--- src/main/java/com/bookstore/management/books/model/Book.java
+++ src/main/java/com/bookstore/management/books/model/Book.java
@@ -1,4 +1,27 @@
 package com.bookstore.management.books.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String ISBN;
+    private String title;
+    private LocalDate publishDate;
+    private String description;
+    private Integer pages;
+    @ManyToOne
+    private Author author;
+    private String imageUrl;
 }
