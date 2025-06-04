d853ccb0decc3de1a9a017cd5b46b38b78c8d471
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-06-04 23:44:48 +0200

    feat: add Author repository with findByName and findByNationality methods

--- src/main/java/com/bookstore/management/books/model/Author.java
+++ src/main/java/com/bookstore/management/books/model/Author.java
@@ -1,12 +1,11 @@
 package com.bookstore.management.books.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,7 +13,18 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Author {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column (name = "name", nullable = false)
     private String name;
+    @Column(name = "nationality", nullable = false)
+    private String nationality;
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    public Author(String name, String nationality, LocalDate dateOfBirth) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+    }
 }
---
+++ src/main/java/com/bookstore/management/books/repository/AuthorRepository.java
@@ -0,0 +1,15 @@
+package com.bookstore.management.books.repository;
+
+import com.bookstore.management.books.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    List<Author> findByNameContainingIgnoreCase(String name);
+
+    List<Author> findByNationality(String nationality);
+}
