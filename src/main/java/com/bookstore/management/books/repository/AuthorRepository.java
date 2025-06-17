e183dc2c11ba1efd16b8e5ce4226790d8395a2ba
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-06-17 22:28:36 +0200

    feat: add findByGender method to AuthorRepository.
    - Implement gender-based search functionality.
    - Enable filtering authors by gender enum values.

--- src/main/java/com/bookstore/management/books/repository/AuthorRepository.java
+++ src/main/java/com/bookstore/management/books/repository/AuthorRepository.java
@@ -12,4 +12,6 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     List<Author> findByNameContainingIgnoreCase(String name);
 
     List<Author> findByNationality(String nationality);
+
+    List<Author> findByGender(Author.Gender gender);
 }
