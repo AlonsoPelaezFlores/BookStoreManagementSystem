19d7d316ad77f585cdecbe8296a8d8ed4a08ea52
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-06-23 23:57:13 +0200

    feat: add BookDto and AuthorDto classes for service layer
    - Create data transfer objects with Lombok annotations
    - Include all necessary fields for book and author operations

--- src/main/java/com/bookstore/management/books/dto/AuthorDto.java
+++ src/main/java/com/bookstore/management/books/dto/AuthorDto.java
@@ -1,4 +1,18 @@
 package com.bookstore.management.books.dto;
 
+import com.bookstore.management.books.model.Author;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class AuthorDto {
+    public String name;
+    public String nationality;
+    public LocalDate dateOfBirth;
+    public Author.Gender gender;
+    public String biography;
+
 }
--- src/main/java/com/bookstore/management/books/dto/BookDto.java
+++ src/main/java/com/bookstore/management/books/dto/BookDto.java
@@ -1,4 +1,20 @@
 package com.bookstore.management.books.dto;
 
+import com.bookstore.management.books.model.Author;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
 public class BookDto {
+
+    private String ISBN;
+    private String title;
+    private LocalDate publishDate;
+    private String description;
+    private Integer pages;
+    private String genre;
+    private Author author;
 }
