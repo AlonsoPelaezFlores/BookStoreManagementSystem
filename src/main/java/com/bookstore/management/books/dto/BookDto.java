9675c3ef6106bcaaee0d5b3a45656018af62008a
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-07-03 23:32:04 +0200

    feat: add @Builder annotation to BookDTO

--- src/main/java/com/bookstore/management/books/dto/BookDto.java
+++ src/main/java/com/bookstore/management/books/dto/BookDto.java
@@ -2,12 +2,14 @@ package com.bookstore.management.books.dto;
 
 import com.bookstore.management.books.model.Author;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class BookDto {
 
     private String ISBN;
