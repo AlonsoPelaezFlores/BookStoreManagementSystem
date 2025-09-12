6eb6d3ea7f4fe69a8618aa01fce58befe47aef65
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-09-12 23:54:09 +0200

    feat: create CustomerRepository interface for data access layer

--- src/main/java/com/bookstore/management/customers/repository/CustomerRepository.java
+++ src/main/java/com/bookstore/management/customers/repository/CustomerRepository.java
@@ -1,4 +1,10 @@
 package com.bookstore.management.customers.repository;
 
-public class CustomerRepository {
+import com.bookstore.management.customers.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
 }
