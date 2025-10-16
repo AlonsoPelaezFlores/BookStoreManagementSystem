e8ee8d7606d0af43ea1638dc74cc645d967271b0
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-10-16 16:22:23 +0200

    refactor: add book summary mapping and remove unnecessary ID mapping
    - Add toBookSummaryDto method for book summary information
    - Remove redundant ID mapping between DTO and entity

--- src/main/java/com/bookstore/management/book/mapper/BookMapper.java
+++ src/main/java/com/bookstore/management/book/mapper/BookMapper.java
@@ -1,5 +1,6 @@
 package com.bookstore.management.book.mapper;
 
+import com.bookstore.management.book.dto.BookSummaryDTO;
 import com.bookstore.management.book.dto.CreateBookDTO;
 import com.bookstore.management.book.model.Book;
 import org.mapstruct.*;
@@ -7,9 +8,11 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
-    @Mapping(target = "id", ignore = true)
+
     Book toEntity(CreateBookDTO createBookDto);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDto(CreateBookDTO createBookDto, @MappingTarget Book book);
+    @Mapping(target = "author", source = "book.author.name")
+    BookSummaryDTO toBookSummaryDTO(Book book);
 
 }
