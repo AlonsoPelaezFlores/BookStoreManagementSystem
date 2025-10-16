e16757987ceba07b621e50cdc5637a64ddd1af9f
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-10-16 18:25:55 +0200

    refactor: improve error response structure
    - Rename ErrorObject to ErrorResponse for clarity
    - Add ValidationErrorResponse class for handling validation errors

---
+++ src/main/java/com/bookstore/management/shared/exception/response/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.bookstore.management.shared.exception.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+    private String code;
+    private Integer status;
+    private String message;
+    private String path;
+}
---
+++ src/main/java/com/bookstore/management/shared/exception/response/ValidationErrorResponse.java
@@ -0,0 +1,25 @@
+package com.bookstore.management.shared.exception.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ValidationErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private Map<String, String> validationErrors;
+
+}
