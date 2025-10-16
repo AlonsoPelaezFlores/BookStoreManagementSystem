0d438f64e6502682d269b2b0748d6c74bf57081c
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-10-17 00:56:28 +0200

    refactor: make CreateAuthorDTO constructor private to fix mapping issues

--- src/main/java/com/bookstore/management/book/dto/CreateAuthorDTO.java
+++ src/main/java/com/bookstore/management/book/dto/CreateAuthorDTO.java
@@ -1,39 +1,34 @@
 package com.bookstore.management.book.dto;
 
 import com.bookstore.management.book.model.Author;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class CreateAuthorDTO {
 
     @NotBlank(message = "The author's name is obligatory")
     @Size(min = 2, max = 100, message = "The name must be between 2 and 100 characters")
-    public String name;
+    private String name;
 
     @NotBlank(message = "The nationality is obligatory")
     @Size(min = 2, max = 50, message = "The nationality must be between 2 and 50 characters")
-    public String nationality;
+    private String nationality;
 
     @NotNull(message = "The date of birth is obligatory")
     @Past(message = "Date of birth must be before today")
-    public LocalDate birthDate;
+    private LocalDate birthDate;
 
     @NotNull(message = "The gender is obligatory")
-    public Author.Gender gender;
+    private Author.Gender gender;
 
     @Size(max = 2000, message = "The biography cannot exceed 2000 characters")
-    public String biography;
+    private String biography;
 
 }
