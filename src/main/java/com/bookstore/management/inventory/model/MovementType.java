e105e02bd79b56145e5fa2b077787236a2ed08b4
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-10-18 04:34:28 +0200

    feat: add new movement types to inventory system

--- src/main/java/com/bookstore/management/inventory/model/MovementType.java
+++ src/main/java/com/bookstore/management/inventory/model/MovementType.java
@@ -10,7 +10,10 @@ public enum MovementType {
     NEGATIVE_ADJUSTMENT("Negative Adjustment"),
     RETURN("Return"),
     RESERVE("Reserve"),
-    RELEASE_RESERVE("Release Reserve");
+    RELEASE_RESERVE("Release Reserve"),
+    INITIAL_INVENTORY("Initial Inventory"),
+    UPDATE_THRESHOLD("Update Threshold"),
+    DISABLE("Disable");
 
     private final String description;
 
