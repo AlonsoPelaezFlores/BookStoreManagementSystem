6ce6f92403fbf743712ae6e6839beb45b7f6a1a8
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-10-23 14:39:40 +0200

    refactor: remove InvalidIsbnException due to redundant DTO validation

--- src/main/java/com/bookstore/management/shared/exception/custom/InvalidIsbnException.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.management.shared.exception.custom;
-
-public class InvalidIsbnException extends BusinessException {
-    public InvalidIsbnException(String message) {
-        super(message);
-    }
-}
