2aaf0100f87acc5681176fc70679d8a99965a975
AlonsoPelaezFlores <alonso18pf@gmail.com>
2025-12-05 17:52:02 +0100

    feature: add repository for sale and salesDetail

--- src/main/java/com/bookstore/management/sales/repository/SaleRepository.java
+++ src/main/java/com/bookstore/management/sales/repository/SaleRepository.java
@@ -1,4 +1,31 @@
 package com.bookstore.management.sales.repository;
 
-public class SaleRepository {
+import com.bookstore.management.sales.model.Sale;
+import com.bookstore.management.sales.model.SalesStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale,Long> {
+
+    @EntityGraph(attributePaths = {"customer","details","details.book"})
+    Optional<Sale> findByCustomerId(Long customerId);
+
+    @Query("SELECT s FROM Sale s WHERE DATE(s.createdAt) BETWEEN :start AND :end")
+    @EntityGraph(attributePaths = {"customer","details","details.book"})
+    List<Sale> findByDateRange(@Param("start") LocalDate start,@Param("end") LocalDate end);
+
+    @EntityGraph(attributePaths = {"customer","details","details.book"})
+    List<Sale> findByStatus(SalesStatus status);
+
+    @EntityGraph(attributePaths = {"customer","details","details.book"})
+    List<Sale> findByCustomerIdAndStatus(Long customerId, SalesStatus status);
 }
